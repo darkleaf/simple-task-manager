bc9b36a7179943848c1ec252bcbaf46f486dcf07
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2012-01-25 12:25:55 +0000

    add tasks to user

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,3 +1,7 @@
 class Story < ActiveRecord::Base
   belongs_to :user
+  belongs_to :responsible_user, :class_name => "User"
+
+  attr_accessible :title, :body, :responsible_user
+ # validates :
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
   has_many :stories, :dependent => :destroy
+  #у пользователя могут быть задачи
+  has_many :tasks, :class_name => "Story", #задачи находятся в таблице Story
+                   :dependent => :nullify, #если удаляем пользователя, вместо него подставляем - NULL
+                   :foreign_key => "responsible_user_id"
+
+
+
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   attr_accessible :name, :email
