bd3ceffac6dfb10ffbb2932a861614104cf1d3ac
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2012-01-25 14:16:03 +0000

    add some tests

--- app/models/story.rb
+++ app/models/story.rb
@@ -3,7 +3,7 @@ class Story < ActiveRecord::Base
   belongs_to :responsible_user, :class_name => "User"
 
   attr_accessible :title, :body, :responsible_user
-  #validates :title, :presence => true,
-  #                  :length   => { :maximum => 256 }    
-  #validates :body, :presence => true  
+  validates :title, :presence => true,
+                    :length   => { :maximum => 256 }    
+  validates :body, :presence => true  
 end
