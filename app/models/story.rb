217758487b70d44b635a09a5713e37df1bb7d4dd
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2012-01-25 13:00:48 +0000

    add factory girls

--- app/models/story.rb
+++ app/models/story.rb
@@ -3,5 +3,7 @@ class Story < ActiveRecord::Base
   belongs_to :responsible_user, :class_name => "User"
 
   attr_accessible :title, :body, :responsible_user
- # validates :
+  #validates :title, :presence => true,
+  #                  :length   => { :maximum => 256 }    
+  #validates :body, :presence => true  
 end
